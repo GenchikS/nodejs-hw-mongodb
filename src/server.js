4c6671e893607522bb2b557b874bd87b36a5640d
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-10-31 12:25:25 +0200

    v.2.1.6

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import pino from 'pino-http';
 import { env } from "./utils/env.js"
 
 // import ContactCollection from "./db/models/Contact.js" 
-import * as contactServices from "./services/contacts.js"  // замість ContactCollection імпортуємо всю логіку з файлу, де він буде шукати певний запит
+import {getContacts, getContactById} from "./services/contacts.js"  // замість ContactCollection імпортуємо всю логіку з файлу, де він буде шукати певний запит
 
 
 const PORT = Number(env(`PORT`, `3000`))
@@ -26,25 +26,30 @@ export const setupServer = () => {
     
 
   app.get(`/contacts`, async (req, res) => {
-    const data = await contactServices.getContacts();
+    const data = await getContacts();
     res.json({
       stasus: 200,
       message: "Successfull find contacts",
       data,
     })
+    return data;
   //  res.json({ massage: 'Hello hw-2' });  необхідно було для перевірки запуску сервера
   });
   
   app.get(`/contacts/:id`, async (req, res) => {
     console.log(`req.params`, req.params); //  зберігаються всі параметри маршрути в req.params
     const { id } = req.params;
-    const data = await contactServices.getContactById(id);
-          if (!data) {
-              res.status(404).json({
-              message: `Contact id= ${id}not found`,
-              });
-          }
     
+    if (id.length !== 24) {
+      // console.log(id.length); //  перевірка довжини ID
+      res.status(404).json({
+        message: `Contact id= ${id} not found`,
+      });
+      return;
+    }
+    
+    const data = await getContactById(id);
+
     res.json({
       stasus: 200,
       message: 'Contact successfull find',
