607525ea110dc90af70f9037bb2d33edd56691d4
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-11-15 01:50:36 +0200

    v.5.1.3

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,23 @@
+import { hathPassword } from "../hashpassword/hathpassword.js";
 import * as authService from "../services/auth.js"
 
 export const registerController = async (req, res) => {
     const data = await authService.registerContact(req.body);
+
+
+
+    //  приклад хешування
+    // console.log('data', data.password);
+    const hath = await hathPassword(data.password);
+    console.log('hath', hath);  //  перевірка паролю з salt
+
+
+
     res.status(201).json({
       status: 201,
       message: 'Successfully registered a user!',
       name: data.name,
       email: data.email,
+      password: hath,
     });
 }
